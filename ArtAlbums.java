dd7e974c42a404e21d6a51ae24467b7f60a3905b
BogdanBarbu17 <106099870+BogdanBarbu17@users.noreply.github.com>
2022-06-19 18:57:41 +0300

    Add files via upload

---
+++ ArtAlbums.java
@@ -0,0 +1,25 @@
+public class ArtAlbums extends Book {
+
+    private String quality;
+
+     public ArtAlbums() {
+
+    }
+
+    public void setQuality(String quality) {
+        this.quality = quality;
+    }
+
+    @Override
+    public String toString() {
+        return     "\n" +
+                "======================================================" +
+                "\nTitle: " + getName() +
+                "\nSize: " + getNumberOfPages() + " pages " +
+                "\nPaper quality: " + quality +
+                "\n" +
+                "======================================================";
+
+    }
+
+}
---
+++ Novels.java
@@ -0,0 +1,27 @@
+
+public class Novels extends Book {
+
+    private String type;
+
+    public Novels() {
+        super();
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+
+    @Override
+    public String toString() {
+        return     "\n" +
+                "======================================================" +
+                "\nTitle: " + getName() +
+                "\nSize: " + getNumberOfPages() + " pages " +
+                "\nType: " + type +
+                "\n" +
+                "======================================================";
+
+    }
+
+}
